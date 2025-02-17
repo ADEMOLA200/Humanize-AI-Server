bd3dd77f51bccbde2b2d079d0619a9dbce85968f
Odukoya Abdullahi Ademola <odukoyaabdullahi01@gmail.com>
2025-02-17 21:17:40 +0100

    humanize server

---
+++ app.py
@@ -0,0 +1,51 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+from huggingface_hub import login
+import torch
+
+load_dotenv()
+
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+if not HF_TOKEN:
+    raise ValueError("Hugging Face token not found. Set HF_TOKEN as an environment variable or in a .env file.")
+
+login(HF_TOKEN)
+
+app = Flask(__name__)
+
+# incase you want to upgrade to Flan-T5-XXL
+model_name = "google/flan-t5-xxl"
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+# Load model and tokenizer with authentication, you can uncomment any one
+# and use them, but "Vamsi/T5_Paraphrase_Paws" is simply preferable so yup!
+# model_name = "t5-small"
+# model_name = "t5-base"
+# model_name = "Vamsi/T5_Paraphrase_Paws"
+
+
+model = T5ForConditionalGeneration.from_pretrained(model_name, token=HF_TOKEN)
+tokenizer = T5Tokenizer.from_pretrained(model_name, token=HF_TOKEN)
+
+@app.route('/paraphrase', methods=['POST'])
+def paraphrase():
+    text = request.json['text']
+    inputs = tokenizer.encode("paraphrase: " + text, return_tensors="pt", max_length=768, truncation=True)
+    outputs = model.generate(
+        inputs,
+        max_length=768,
+        min_length=200,  # this will ensures at least 200 tokens are generated
+        do_sample=True,
+        num_beams=5,
+        num_return_sequences=1,
+        temperature=0.7, #1.5
+        top_p=0.9
+    )
+    return jsonify({'paraphrased': tokenizer.decode(outputs[0], skip_special_tokens=True)})
+
+if __name__ == '__main__':
+    app.run(port=5001)
---
+++ makefile
@@ -0,0 +1,26 @@
+uvicorn:
+	uvicorn nlp_server:app --host 0.0.0.0 --port 8000 --reload
+
+uvicorn-reload:
+	uvicorn nlp_server:app --reload
+
+# DP SK
+start-server:
+	python app.py
+
+# Install dependencies
+requirements:
+	pip install -r requirements.txt
+
+activate-venv:
+	source .venv/bin/activate
+
+create-venv-environment:
+	python3 -m venv .venv
+
+install-dependencies:
+	pip install flask transformers huggingface_hub torch
+
+# Start the server
+# start-server:
+# 	python models/openai_t5_server.py
